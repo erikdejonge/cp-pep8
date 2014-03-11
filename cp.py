b23be8344574c4746c7e64a5729ce3a1b3663f44
Erik de Jonge <erik@a8.nl>
2014-03-11 10:16:25 +0100

    Tue Mar 11 10:16:25 CET 2014

--- cp.py
+++ cp.py
@@ -649,6 +649,8 @@ def coffee_script_pretty_printer(add_double_enter, add_enter, debuginfo, first_m
             debuginfo += " on global"
             if not global_line(prev_line):
                 add_double_enter = True
+            if prev_line.strip().startswith(")"):
+                add_double_enter = True
         if scoped > 1:
             debuginfo += " on prev scope"
             add_enter = True
