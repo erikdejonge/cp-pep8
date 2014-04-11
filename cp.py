5e101d68581d53f300c476787baab0afc861bb1d
Erik de Jonge <erik@a8.nl>
2014-04-11 14:37:44 +0200

    Fri Apr 11 14:37:44 CEST 2014

--- cp.py
+++ cp.py
@@ -1248,7 +1248,7 @@ def sanatize_line(line, next_line):
     @param next_line:
     @return: @rtype:
     """
-    if not (line.strip().endswith(",") or ")" in next_line) and not in_test([")", "=>", "!=", "==", "$(", "?", "ng-", "trim", "strip", "match", "split", "input", "type=", "/=", "\=", ":", "replace", "element", "if ", "b64", "padding"], line):
+    if not (line.strip().endswith(",") or ")" in next_line) and not in_test([")", "=>", "!=", "hotkeys", "==", "$(", "?", "ng-", "trim", "strip", "match", "split", "input", "type=", "/=", "\=", ":", "replace", "element", "if ", "b64", "padding"], line):
         line = line.replace("=>", "@>").replace("( ", "(").replace("=", " = ").replace("  =", " =").replace("=  ", "= ").replace("@>", "=>").replace("< =", "<=").replace("> =", ">=").replace("+ =", "+=").replace("- =", "-=").replace("* =", "*=").replace("! =", "!=").replace('(" = ")', '("=")').replace('+ " = "', '+ "="')
         if not "+=" in line and not "++" in line:
             line = line.replace("+", " + ")
